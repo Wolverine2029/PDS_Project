e2b577ce0db0e0bbb4a4e1906a11982659a13988
nabilahaahim14 <62465875+nabilahaahim14@users.noreply.github.com>
2022-10-23 18:05:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 
+
+
+
 #Main Page
 EXAMPLE_NO = 3
 
@@ -71,10 +74,34 @@ if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 
-supervised = st.selectbox('Select one option:', ['', 'Summary of my Data', 'Clean the data', 'Data Analysis', 'Data Distribution'], format_func=lambda x: 'Select an option' if x == '' else x)
+supervised = st.selectbox('Select one option:', ['', 'Summary of my Data', 'Check for Null Values in the data','Check for Percentage of Missing Values with greater than 80% in the data', 'Data Analysis', 'Data Distribution'], format_func=lambda x: 'Select an option' if x == '' else x)
 
 if supervised:
     st.success('Yay! 🎉')
 else:
     st.warning('No option is selected')
 # print("you selected: ",option)
+if supervised == 'Summary of my Data':
+    summary = dataframe.describe()
+    st.write(summary)
+if supervised == 'Check for Null Values in the data':
+    nullData = dataframe.isnull().sum()
+    st.write(nullData)
+# if supervised == 'Check for % of Missing Values with >= 80% in the data':
+#     NAN = [(i, dataframe[i].isna().mean() * 100) for i in dataframe]
+    # NAN = pd.DataFrame(NAN, columns=["column_name", "percentage"])
+    # NAN = NAN[NAN.percentage > 50]
+    # NAN.sort_values("percentage", ascending=False)
+    # st.write(NAN)
+    cleanDF_option = st.selectbox('Select one option:', ['', 'Remove the entire row containing null value','Replace the null value with your desired value'],
+                              format_func=lambda x: 'Select an option' if x == '' else x)
+    if cleanDF_option == 'Remove rows with Nulls values':
+        pass
+        # cleandf = dataframe[]
+    #     remove = NAN
+    #     removed_na_data = dataframe.drop(remove, inplace=True, axis = 1)
+    #     st.write(removed_na_data)
+    # if option01 == 'Replace the null value with your desired value':
+    #
+    #     option02 = st.selectbox('Select one option:', ['', 'Replace with zero','Replace with mean','Replace with median','Replace with mode'],
+    #                             format_func=lambda x: 'Select an option' if x == '' else x)
