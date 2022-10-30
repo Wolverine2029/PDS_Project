438ff5256339a32f7365f19bd6968ed5cbbc3859
nabilahaahim14 <62465875+nabilahaahim14@users.noreply.github.com>
2022-10-30 13:08:50 -0500

    Update file till the cleaning part

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 import streamlit as st
 import pandas as pd
+import pandas_profiling
+import seaborn as sns
+import tkinter
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
 from streamlit_option_menu import option_menu
+from streamlit_pandas_profiling import st_profile_report
 
 
-
-
-#Main Page
+# Main Page
 EXAMPLE_NO = 3
 
 
@@ -63,8 +68,6 @@ st.markdown(f""" <style>
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
 
-
-
 st.title("""
 DANA - A Fun place to play around with Machine Learning algorithms! 
 """)
@@ -73,35 +76,96 @@ uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+    numeric_cols = dataframe.select_dtypes(include=['number']).columns
+    non_numeric_cols = dataframe.select_dtypes(exclude=['number']).columns
 
-supervised = st.selectbox('Select one option:', ['', 'Summary of my Data', 'Check for Null Values in the data','Check for Percentage of Missing Values with greater than 80% in the data', 'Data Analysis', 'Data Distribution'], format_func=lambda x: 'Select an option' if x == '' else x)
+supervised = st.selectbox('Select one option:',
+                          ['', 'Data Report', 'Summary of my Data', 'Check for Null Values in the data',
+                            'Data Analysis','Data Distribution'], format_func=lambda x: 'Select an option' if x == '' else x)
 
 if supervised:
     st.success('Yay! 🎉')
 else:
     st.warning('No option is selected')
 # print("you selected: ",option)
+if supervised == 'Data Report':
+    pr = dataframe.profile_report()
+    st_profile_report(pr)
+
 if supervised == 'Summary of my Data':
     summary = dataframe.describe()
     st.write(summary)
+
 if supervised == 'Check for Null Values in the data':
     nullData = dataframe.isnull().sum()
     st.write(nullData)
-# if supervised == 'Check for % of Missing Values with >= 80% in the data':
-#     NAN = [(i, dataframe[i].isna().mean() * 100) for i in dataframe]
-    # NAN = pd.DataFrame(NAN, columns=["column_name", "percentage"])
-    # NAN = NAN[NAN.percentage > 50]
-    # NAN.sort_values("percentage", ascending=False)
-    # st.write(NAN)
-    cleanDF_option = st.selectbox('Select one option:', ['', 'Remove the entire row containing null value','Replace the null value with your desired value'],
-                              format_func=lambda x: 'Select an option' if x == '' else x)
-    if cleanDF_option == 'Remove rows with Nulls values':
-        pass
-        # cleandf = dataframe[]
-    #     remove = NAN
-    #     removed_na_data = dataframe.drop(remove, inplace=True, axis = 1)
-    #     st.write(removed_na_data)
-    # if option01 == 'Replace the null value with your desired value':
+    nullDataPercentage = dataframe.isnull().mean()
+    Clean_DF = dataframe.copy()
+
+    cleanDF_option = st.selectbox('Select one option:',
+                                  ['', 'Remove columns with Nulls values', 'Impute the null with constant values',
+                                   'Impute the null with statistics'],
+                                  format_func=lambda x: 'Select an option' if x == '' else x)
+    if cleanDF_option == 'Remove columns with Nulls values':
+        # Removing the columns with null values but only those columns that have null value percentage greater than 30% in the column.
+        nullDataPercentage[nullDataPercentage > .3]
+        CleanDF_less_missing_columns = dataframe.loc[:,
+                                       nullDataPercentage <= .3].copy()  # equivalent to df.drop(columns=pct_missing[pct_missing > .3].index)
+        # CleanDF_less_missing_columns.shape
+        st.write("The updated DataSet is")
+        st.write(CleanDF_less_missing_columns.head())
+    if cleanDF_option == 'Impute the null with constant values':
+        CleanDF_replace_constant = dataframe.copy()
+        # numeric_cols = dataframe.select_dtypes(include=['number']).columns
+        # non_numeric_cols = dataframe.select_dtypes(exclude=['number']).columns
+        CleanDF_replace_constant[numeric_cols] = CleanDF_replace_constant[numeric_cols].fillna(0)
+        CleanDF_replace_constant[non_numeric_cols] = CleanDF_replace_constant[non_numeric_cols].fillna('NA')
+        CleanDF_constant = CleanDF_replace_constant.head()
+
+        # st.write(CleanDF_constant)
+    if cleanDF_option == 'Impute the null with statistics':
+        CleanDF_replace_statistics = dataframe.copy()
+
+        cleanDF_option_replace = st.selectbox('Select one option:',
+                                              ['', 'Mean', 'Median', 'Mode'],
+                                              format_func=lambda x: 'Select an option' if x == '' else x)
+
+        st.write("This option only works on Numerical Columns")
+        if cleanDF_option_replace == 'Mean':
+            mean = CleanDF_replace_statistics[numeric_cols].mean()
+            CleanDF_replace_statistics[numeric_cols] = CleanDF_replace_statistics[numeric_cols].fillna(mean)
+            # st.write(CleanDF_replace_statistics.head())
+        if cleanDF_option_replace == 'Median':
+            med = CleanDF_replace_statistics[numeric_cols].median()
+            CleanDF_replace_statistics[numeric_cols] = CleanDF_replace_statistics[numeric_cols].fillna(med)
+            # st.write(CleanDF_replace_statistics.head())
+        if cleanDF_option_replace == 'Mode':
+            mode = CleanDF_replace_statistics[numeric_cols].mode()
+            CleanDF_replace_statistics[numeric_cols] = CleanDF_replace_statistics[numeric_cols].fillna(mode)
+            # st.write(CleanDF_replace_statistics.head())
+
+
+    if (cleanDF_option == 'Remove columns with Nulls values'):
+        Clean_DF = CleanDF_less_missing_columns.copy()
+    if (cleanDF_option == 'Impute the null with constant values'):
+        Clean_DF = CleanDF_replace_constant.copy()
+    if (cleanDF_option == 'Impute the null with statistics'):
+        Clean_DF = CleanDF_replace_statistics.copy()
+    if ((cleanDF_option == 'Impute the null with statistics') and (cleanDF_option_replace == 'Mean')):
+        Clean_DF = CleanDF_replace_statistics.copy()
+    if ((cleanDF_option == 'Impute the null with statistics') and (cleanDF_option_replace == 'Median')):
+        Clean_DF = CleanDF_replace_statistics.copy()
+    if ((cleanDF_option == 'Impute the null with statistics') and (cleanDF_option_replace == 'Mode')):
+        Clean_DF = CleanDF_replace_statistics.copy()
+
+    st.write("Please find the cleaned dataset below:")
+    st.write(Clean_DF.head())
+
+    # Clean_DF.corr()
+    # # plotting correlation heatmap
+    # dataplot = sns.heatmap(Clean_DF.corr(), cmap="YlGnBu", annot=True)
     #
-    #     option02 = st.selectbox('Select one option:', ['', 'Replace with zero','Replace with mean','Replace with median','Replace with mode'],
-    #                             format_func=lambda x: 'Select an option' if x == '' else x)
+    # # displaying heatmap
+    # s = plt.show()
+    # st.write(s)
+
